3fd773b289842f54731f7b175c448738bdc18b84
PerryPeng <bdfpeng@163.com>
2018-10-04 11:18:49 +0800

    Create decode.py

---
+++ communicationLab/decode.py
@@ -0,0 +1,20 @@
+import base64
+import os
+import sys
+
+infile_name = sys.argv[1]
+
+infile_handle = open(infile_name, 'r')
+
+infile_content = infile_handle.read()
+
+outfile_content =  base64.b64decode(infile_content)
+
+outfile_name = 'dec' + infile_name
+
+outfile_handle = open(outfile_name, 'w')
+
+outfile_handle.write(outfile_content)
+
+infile_handle.close()
+outfile_handle.close()
